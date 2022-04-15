1c09f9ea2720f9dacf1550cf126f20f6c16ed3b2
Mattia <mattia@MacBook-Pro-di-Mattia.local>
2022-04-15 19:43:05 +0200

    Removed a lot of unused auxiliary functions

--- Interval.hs
+++ Interval.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
 
 module Interval where
@@ -19,15 +18,10 @@ instance Show Interval where
     show (IVar s) = show s
 
 instance Ord Interval where
-    compare IZero (IVar s) = LT
-    compare (IVar s) IOne = LT
-    compare IZero IOne = LT
-    compare IZero IZero = EQ
-    compare IOne IOne = EQ
-    compare (IVar s1) (IVar s2) = compare s1 s2
-    compare (IVar s) IZero = GT
-    compare IOne (IVar s) = GT
-    compare IOne IZero = GT
+    IZero  <= IVar _ = True
+    IZero  <= IOne   = True
+    IVar _ <= IOne   = True
+    _      <= _      = False
 
 data Formula = FTrue
              | FFalse
@@ -51,25 +45,12 @@ instance Show Formula where
 type DNFFormula = (Set (Set (Ident,Interval)))
 
 trueDNF :: DNFFormula
-trueDNF = Set.singleton (Set.empty)
+trueDNF = Set.singleton Set.empty
 
 showDNF :: DNFFormula -> String
 showDNF dnf = intercalate " \\/ " (map showConj (toList dnf))
     where showConj :: Set (Ident,Interval) -> String
           showConj conj = "(" ++ intercalate " /\\ " (map (\(s,i) -> show s ++ " = " ++ show i) (toList conj)) ++ ")"
-
-
-getFormulaNames :: Formula -> [Ident]
-getFormulaNames = nub . getFormulaNames' --remove duplicates
-    where getFormulaNames' :: Formula -> [Ident]
-          getFormulaNames' ff = case ff of
-                FTrue -> []
-                FFalse -> []
-                Eq0 s -> [s]
-                Eq1 s -> [s]
-                Diag s1 s2 -> [s1,s2]
-                ff1 :/\: ff2 -> getFormulaNames' ff1 ++ getFormulaNames' ff2
-                ff1 :\/: ff2 -> getFormulaNames' ff1 ++ getFormulaNames' ff2
 
 makeOr :: Formula -> Formula -> Formula
 makeOr FFalse ff2 = ff2
@@ -93,20 +74,6 @@ foldAnd :: [Formula] -> Formula
 foldAnd [] = FTrue
 foldAnd ffs = foldl1 makeAnd ffs
 
-decomposeOr :: Formula -> [Formula]
-decomposeOr (ff1 :\/: ff2) = decomposeOr ff1 ++ decomposeOr ff2
-decomposeOr FFalse = []
-decomposeOr ff = [ff]
-
-decomposeAnd :: Formula -> [Formula]
-decomposeAnd (ff1 :/\: ff2) = decomposeAnd ff1 ++ decomposeAnd ff2
-decomposeAnd FTrue = []
-decomposeAnd ff = [ff]
-
-getBoundary :: Formula -> Formula
-getBoundary ff = foldOr disjunctions
-    where names = getFormulaNames ff
-          disjunctions = map (\s -> (Eq0 s) :\/: (Eq1 s)) names
 
 foldOrDNF :: [Set (Ident,Interval)] -> DNFFormula
 foldOrDNF conjs = if Set.empty `elem` conjs then
@@ -130,13 +97,13 @@ toDNF ff = case ff of
 
 
 fromDNF :: DNFFormula -> Formula
-fromDNF (dnf) = foldOr $ map (foldAnd . (map coupleToAtom) . toList) (toList dnf)
-    where coupleToAtom (s,IZero) = (Eq0 s)
-          coupleToAtom (s,IOne) = (Eq1 s)
-          coupleToAtom (s1,IVar s2) = (Diag s1 s2)
+fromDNF (dnf) = foldOr $ map (foldAnd . map coupleToAtom . toList) (toList dnf)
+    where coupleToAtom (s,IZero) = Eq0 s
+          coupleToAtom (s,IOne) = Eq1 s
+          coupleToAtom (s1,IVar s2) = Diag s1 s2
 
 simplifyDNF :: DNFFormula -> DNFFormula
-simplifyDNF (dnf) = if Set.empty `Set.member` dnf then
+simplifyDNF dnf = if Set.empty `Set.member` dnf then
         trueDNF
     else foldOrDNF $ concatMap (toConj . toDirEnv . toList) (toList dnf')
     where
@@ -172,7 +139,9 @@ singleSubst ff (s,i) = case ff of
                 IVar x -> Eq1 x
         else
             Eq1 s'
-    Diag s1 s2 -> if s == s1 then 
+    Diag s1 s2 -> if s1 == s2 then --silly case (i = i)
+            Diag s1 s2
+        else if s == s1 then 
             case i of 
                 IZero  -> Eq0 s2
                 IOne   -> Eq1 s2
@@ -209,17 +178,6 @@ imp ff1 ff2 = all (\conj1 -> any (\conj2 -> conj2 `Set.isSubsetOf` conj1) dnf2) 
 
 equalFormulas :: Formula -> Formula -> Bool
 equalFormulas ff1 ff2 = (simplifyDNF . toDNF) ff1 == (simplifyDNF . toDNF) ff2
-
-checkCongruence :: Formula -> Formula -> Formula -> Bool
-checkCongruence ff ff1 ff2 = checkCongruenceDNF (toDNF ff) ff1 ff2
-
-checkCongruenceDNF :: DNFFormula -> Formula -> Formula -> Bool
-checkCongruenceDNF dnf ff1 ff2 = all (\conj -> checkCongruenceConj (toList conj) ff1 ff2) (toList dnf)
-    where
-        checkCongruenceConj [] ff' ff'' = equalFormulas ff' ff''
-        checkCongruenceConj (atom:conj) ff' ff'' = checkCongruenceConj conj (singleSubst ff' (s,i)) (singleSubst ff'' (s,i))
-            where (s,i) = atom
-
 
 {- Directions enviroment -}
 
@@ -303,14 +261,9 @@ toSubsts (zeros,ones,diags) = substs0 ++ substs1 ++ substsd
 toDirEnv :: [(Ident,Interval)] -> DirEnv
 toDirEnv conj = addConj conj emptyDirEnv
 
-fromDirEnv :: DirEnv -> [(Ident,Interval)]
-fromDirEnv = toSubsts
-
-(+++) :: DirEnv -> DirEnv -> DirEnv
-dirs1 +++ dirs2 = toDirEnv $ fromDirEnv dirs1 ++ fromDirEnv dirs2
-
 {- Examples -}
 
+{-
 i = Ident "i"
 j = Ident "j"
 k = Ident "k"
@@ -323,4 +276,4 @@ f5 = ((Eq0 i) :\/: (Eq1 i)) :/\: ((Eq0 j) :\/: ((Eq1 j) :/\: (Eq0 j)))
 f6 = ((Eq0 i) :\/: (Eq1 i)) :/\: ((Eq0 j) :\/: ((Eq1 i) :/\: (Eq0 j)))
 f7 = FTrue :/\: ((Eq0 i) :/\: (Eq1 j))
 f8 = FFalse :\/: FTrue :\/: ((Eq0 i) :/\: (Eq1 j))
-
+-}
